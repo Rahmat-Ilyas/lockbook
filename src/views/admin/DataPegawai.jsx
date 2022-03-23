e747ab80cd6830f4e2def2567f505b555071b187
rahmat-ilyas <rahmat.ilyas142@gmail.com>
2022-03-24 04:57:28 +0800

    edit satu

--- src/views/admin/DataPegawai.jsx
+++ src/views/admin/DataPegawai.jsx
@@ -1,13 +1,73 @@
 import React, { Component, useState, useEffect } from 'react';
-import $ from 'jquery';
+import $, { each } from 'jquery';
+import { toast } from 'react-toastify';
 import DataTable from 'datatables.net';
 import { db } from '../../config/firebase.js';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, updateDoc, query, where } from "firebase/firestore";
 import Layout from "./Layout";
 
 export default class DataPegawai extends Component {
-    async componentDidMount() {
+    state = {
+        uid: '',
+        nip: '',
+        nama: '',
+        telepon: '',
+        alamat: '',
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        let data = {};
+        for (let [key, val] of formData.entries()) {
+            data[key] = val;
+        }
+
+        $('.btn-submit').html('Update <i class="fa fa-spinner fa-spin"></i>').attr('disabled', '');
+
+        try {
+            // const result = query(collection(db, "pegawai"), where("nip", "==", nip));
+            // const result = await getDoc(query(doc(db, "pegawai"), where("nip", "==", nip)));
+
+
+            // const result = await getDocs(res);
+            const result = doc(db, "pegawai", data.id);
+
+            await updateDoc(result, {
+                nip: data.nip,
+                nama: data.nama,
+                telepon: data.telepon,
+                alamat: data.alamat,
+                // password: data.nip,
+            });
+
+            $('.modal').hide();
+            this.notify('success', 'Data pegawai baru berhasil ditambah');
+            this.getData();
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+        $('.btn-submit').html('Update').removeAttr('disabled');
+
+    }
+
+    notify = (status, message) => {
+        var config = {
+            theme: "colored",
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 3000
+        };
+        if (status == 'success') toast.success(message, config);
+        else if (status == 'info') toast.info(message, config);
+        else if (status == 'warn') toast.warn(message, config);
+        else if (status == 'error') toast.error(message, config);
+    }
+
+    async getData() {
         var table = $('#tablePegawai').DataTable();
+        table.clear().draw();
 
         const result = await getDocs(collection(db, "pegawai"));
         let no = 1;
@@ -19,11 +79,24 @@ export default class DataPegawai extends Component {
                 2: res.nama,
                 3: res.telepon,
                 4: res.alamat,
-                5: `<button class="btn btn-success"><i class="fa fa-edit"></i> Edit</button>
+                5: `<button class="btn btn-success btn-edit" data-toggle="modal" data-target="#modal-edit" data-id=` + doc.id + `><i class="fa fa-edit"></i> Edit</button>
                     <button class="btn btn-danger"><i class="fa fa-trash"></i> Hapus</button>`
             }).draw();
             no += 1;
         });
+
+        $('.btn-edit').click(async function () {
+            let id = $(this).attr('data-id');
+            $('#uid').val(id);
+            const result = await getDoc(doc(db, "pegawai", id));
+            $.each(result.data(), function (key, val) {
+                $('input[name="' + key + '"], textarea[name="' + key + '"]').val(val);
+            });
+        });
+    }
+
+    async componentDidMount() {
+        this.getData();
     }
 
     render() {
@@ -72,6 +145,51 @@ export default class DataPegawai extends Component {
 
                         </div>
                     </div >
+
+                    {/* Modal Edit */}
+                    <div className="modal fade" id="modal-edit" tabIndex={-1} role="dialog" aria-labelledby="modal-editLabel" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="modal-editLabel">Edit Data</h5>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div className="form-group row">
+                                            <label className="col-md-3">B/N</label>
+                                            <div className="col-md-9">
+                                                <input type="hidden" name="id" id="uid" />
+                                                <input type="number" name="nip" className="form-control" required placeholder="B/N..." />
+                                            </div>
+                                        </div>
+                                        <div className="form-group row">
+                                            <label className="col-md-3">Nama Lengkap</label>
+                                            <div className="col-md-9">
+                                                <input type="text" name="nama" className="form-control" required placeholder="Nama Lengkap..." />
+                                            </div>
+                                        </div>
+                                        <div className="form-group row">
+                                            <label className="col-md-3">Telepon</label>
+                                            <div className="col-md-9">
+                                                <input type="text" name="telepon" className="form-control" required placeholder="Telepon..." />
+                                            </div>
+                                        </div>
+                                        <div className="form-group row">
+                                            <label className="col-md-3">Alamat</label>
+                                            <div className="col-md-9">
+                                                <textarea name="alamat" className="form-control" required rows="5" placeholder="Alamat..." />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                        <button type="submit" className="btn btn-primary btn-submit">Update</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
                 </Layout >
             </div >
         );
